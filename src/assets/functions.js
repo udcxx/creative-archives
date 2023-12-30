e704971f36b55346dd3617ee716b9643e8161e62
udcxx <me@udcxx.me>
2023-12-31 02:26:05 +0900

    adminまわり調整

--- src/assets/functions.js
+++ src/assets/functions.js
@@ -259,7 +259,7 @@ function dateString(targetDate,isIncludeTime) {
  * ランダムなコードを生成します
  */
 function code() {
-    const character = ["A", "B", "C", "D", "E", "F", "G", "H", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "j", "k", "m", "n", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "+", "-", "~", "@", "*", "="];
+    const character = ["A", "B", "C", "D", "E", "F", "G", "H", "J", "K", "L", "M", "N", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "j", "k", "m", "n", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "+", "-", "~", "@", "*", "="];
     const random = Math.floor(Math.random()*(16 - 6) + 6);
     let codeStrings = "";
     for (let index = 0; index < random; index++) {
